379ef91b56aca6691f8e7b8d925edaaea9e00849
TomE <tommevans210@gmail.com>
2022-12-13 14:26:10 +0700

    first or second

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-console.log('hi');
+const pounds = document.querySelector('.pounds'),
+ kilograms = document.querySelector('.kilograms'),
+ grams = document.querySelector('.grams'),
+ ounces = document.querySelector('.ounces'),
+
+
+//  pounds converter
+
+pounds.addEventListener
